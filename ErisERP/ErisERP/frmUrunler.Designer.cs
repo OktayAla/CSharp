1d2bdb206d4ba4ba4c207ff39ba18ae1a9688386
Oktay Ala <oktayala@gmail.com>
2025-08-05 21:07:29 +0300

    etc

---
+++ ErisERP/ErisERP/frmUrunler.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ErisERP
+{
+    partial class frmUrunler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Button btnListele;
+            dgvUrunler = new DataGridView();
+            btnEkle = new Button();
+            btnListele = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvUrunler).BeginInit();
+            SuspendLayout();
+            // 
+            // btnListele
+            // 
+            btnListele.Location = new Point(12, 12);
+            btnListele.Name = "btnListele";
+            btnListele.Size = new Size(94, 29);
+            btnListele.TabIndex = 1;
+            btnListele.Text = "Listele";
+            btnListele.UseVisualStyleBackColor = true;
+            btnListele.Click += btnListele_Click;
+            // 
+            // dgvUrunler
+            // 
+            dgvUrunler.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvUrunler.Location = new Point(12, 303);
+            dgvUrunler.Name = "dgvUrunler";
+            dgvUrunler.RowHeadersWidth = 51;
+            dgvUrunler.Size = new Size(1158, 188);
+            dgvUrunler.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            btnEkle.Location = new Point(12, 47);
+            btnEkle.Name = "btnEkle";
+            btnEkle.Size = new Size(94, 29);
+            btnEkle.TabIndex = 2;
+            btnEkle.Text = "Ekle";
+            btnEkle.UseVisualStyleBackColor = true;
+            btnEkle.Click += btnEkle_Click;
+            // 
+            // frmUrunler
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1182, 503);
+            Controls.Add(btnEkle);
+            Controls.Add(btnListele);
+            Controls.Add(dgvUrunler);
+            Name = "frmUrunler";
+            Text = "frmUrunler";
+            ((System.ComponentModel.ISupportInitialize)dgvUrunler).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvUrunler;
+        private Button btnEkle;
+    }
+}
---
+++ ErisERP/ErisERP/frmUrunler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ErisERP
+{
+    public partial class frmUrunler : Form
+    {
+        public frmUrunler()
+        {
+            InitializeComponent();
+        }
+
+        private void frmUrunler_Load(object sender, EventArgs e)
+        {
+            UrunListele();
+        }
+
+        private void UrunListele()
+        {
+            using (SqlConnection conn = DbHelper.GetConnection())
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Urunler", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvUrunler.DataSource = dt;
+            }
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            UrunListele();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = DbHelper.GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Urunler (UrunKodu, UrunAdi, BirimFiyat, StokMiktari) VALUES (@kod, @ad, @fiyat, @stok)", conn);
+                cmd.Parameters.AddWithValue("@kod", Guid.NewGuid().ToString().Substring(0, 8));
+                cmd.Parameters.AddWithValue("@ad", "Yeni Ürün");
+                cmd.Parameters.AddWithValue("@fiyat", 150);
+                cmd.Parameters.AddWithValue("@stok", 25);
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Ürün eklendi");
+            UrunListele();
+        }
+    }
+}
