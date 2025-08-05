cc632ee7b2193c025001a48603803a258db30dff
Oktay Ala <oktayala@gmail.com>
2025-08-05 21:07:10 +0300

    etc

---
+++ ErisERP/ErisERP/frmMusteriler.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ErisERP
+{
+    partial class frmMusteriler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Button btnListele;
+            btnEkle = new Button();
+            dgvMusteriler = new DataGridView();
+            btnListele = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvMusteriler).BeginInit();
+            SuspendLayout();
+            // 
+            // btnListele
+            // 
+            btnListele.Location = new Point(12, 12);
+            btnListele.Name = "btnListele";
+            btnListele.Size = new Size(94, 29);
+            btnListele.TabIndex = 5;
+            btnListele.Text = "Listele";
+            btnListele.UseVisualStyleBackColor = true;
+            btnListele.Click += btnListele_Click;
+            // 
+            // btnEkle
+            // 
+            btnEkle.Location = new Point(12, 47);
+            btnEkle.Name = "btnEkle";
+            btnEkle.Size = new Size(94, 29);
+            btnEkle.TabIndex = 6;
+            btnEkle.Text = "Ekle";
+            btnEkle.UseVisualStyleBackColor = true;
+            btnEkle.Click += btnEkle_Click;
+            // 
+            // dgvMusteriler
+            // 
+            dgvMusteriler.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvMusteriler.Location = new Point(12, 303);
+            dgvMusteriler.Name = "dgvMusteriler";
+            dgvMusteriler.RowHeadersWidth = 51;
+            dgvMusteriler.Size = new Size(1158, 188);
+            dgvMusteriler.TabIndex = 7;
+            // 
+            // frmMusteriler
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1182, 503);
+            Controls.Add(dgvMusteriler);
+            Controls.Add(btnEkle);
+            Controls.Add(btnListele);
+            Name = "frmMusteriler";
+            Text = "frmMusteriler";
+            ((System.ComponentModel.ISupportInitialize)dgvMusteriler).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnEkle;
+        private DataGridView dgvMusteriler;
+    }
+}
---
+++ ErisERP/ErisERP/frmMusteriler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ErisERP
+{
+    public partial class frmMusteriler : Form
+    {
+        public frmMusteriler()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMusteriler_Load(object sender, EventArgs e)
+        {
+            MusteriListele();
+        }
+
+        private void MusteriListele()
+        {
+            using (SqlConnection conn = DbHelper.GetConnection())
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Musteriler", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvMusteriler.DataSource = dt;
+            }
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            MusteriListele();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = DbHelper.GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Musteriler (MusteriKodu, FirmaAdi) VALUES (@kod, @firma)", conn);
+                cmd.Parameters.AddWithValue("@kod", Guid.NewGuid().ToString().Substring(0, 8));
+                cmd.Parameters.AddWithValue("@firma", "Firma B");
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Müşteri eklendi");
+            MusteriListele();
+        }
+    }
+}
